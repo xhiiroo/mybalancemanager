305bce9ce12b740935d8bd968f71e4130d9911ec
William <xhiiroo.project@gmail.com>
2024-09-25 20:47:41 +0700

    Modify warna tampilan mouseover dan mouseout

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ const rows = document.querySelectorAll("tbody tr");
 
 rows.forEach(row => {
     row.addEventListener('mouseover', () => {
-        row.classList.add('highlight');
+        row.classList.add('bg-success', 'bg-gradient');
     });
 
     row.addEventListener('mouseout', () => {
-        row.classList.remove('highlight');
+        row.classList.remove('bg-success', 'bg-gradient');
+    });
+});
+
+const ahover = document.querySelectorAll("tbody tr td a");
+
+ahover.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.classList.add('text-info');
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.classList.remove('text-info');
     });
 });
